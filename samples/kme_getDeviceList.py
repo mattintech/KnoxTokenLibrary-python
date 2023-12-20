453120924108ecf33ee1d5d75e6ad412de61221f
Matt Hills <mattintech@gmail.com>
2023-12-19 20:00:19 -0500

    changed project structure & updated docs

---
+++ samples/kme_getDeviceList.py
@@ -0,0 +1,28 @@
+from KnoxAccessToken import getAccessToken
+import requests
+import json
+
+clientIdFile = "clientId.json"
+with open(clientIdFile, "r") as file:
+    clientIds = json.load(file)
+
+clientId = clientIds['kme']
+signedClientId = getAccessToken(clientId)
+
+baseUrl = "https://us-kcs-api.samsungknox.com/kcs/v1/"
+
+def getDeviceList():
+    api = baseUrl+"kme/devices/list"
+    headers = {
+        'Content-Type': 'application/json',
+        'x-knox-apitoken': signedClientId
+    }
+
+    response = requests.get(api, headers=headers)
+    if response.status_code == 200:
+        print(response.text)
+    else:
+        print(f"Request failed. Status code: {response.status_code}, Response: {response.text}")
+
+getDeviceList()
+
